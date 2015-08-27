0d31141972908d099ac05eedc18dcabef0c5e28b
Seiji Mendoza <srm5149@gmail.com>
2015-08-27 19:22:49 -0400

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+import webapp2
+
+form="""
+<form action="http://www.google.com/search">
+	<input name="q">
+	<input type="submit">
+</form>
+
+"""
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
